e8a92910bdd05fce61083da7d69b534d41a54d50
sathyanarendrareddybudati <sathyanarendrareddybudati@gmail.com>
2023-09-11 18:15:23 +0530

    database changes

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import pymysql
 
-SQLALCHEMY_DATABASE_URL = "mysql://bros:Satya199$$@127.0.0.1/bros"
+pymysql.install_as_MySQLdb()
+
+# db_url = "mysql+pymysql://username:password@localhost/database_name"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Nava199$$@localhost/bee_bros"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
